155de0b71642dd4afc516f4f3fa260dde5571d59
Paloma Calado <palomacalado@dcc.ufrj.br>
2026-09-03 21:35:23 -0300

    chore: remove obsolete Neon config wrapper

--- src/lib/neonConfig.js
+++
@@ -1,6 +0,0 @@
-export const neonConfig = {
-  authUrl: process.env.REACT_APP_NEON_AUTH_URL || "",
-  dataApiUrl: process.env.REACT_APP_NEON_DATA_API_URL || "",
-};
-
-export const neonConfigured = Boolean(neonConfig.authUrl && neonConfig.dataApiUrl);
